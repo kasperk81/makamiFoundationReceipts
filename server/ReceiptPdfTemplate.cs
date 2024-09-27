6fd33101a1dd60cda95e45be6c926159aae98fb6
ckepfordmakami <ckepford@makamicollege.com>
2024-09-27 11:44:53 -0700

    Initial Commit

---
+++ server/ReceiptPdfTemplate.cs
@@ -0,0 +1,37 @@
+﻿using QuestPDF.Fluent;
+using QuestPDF.Helpers;
+using QuestPDF.Infrastructure;
+
+namespace StripeExample
+{
+    public class ReceiptPdfTemplate : IDocument
+    {
+        public void Compose(IDocumentContainer container)
+        {
+            container.Page(page =>
+            {
+                page.Margin(50);
+                page.Size(PageSizes.A4.Landscape());
+                page.Header()
+                    //.Section("Receipt")
+                    .AlignCenter()
+                    .ShowOnce()
+                    .Element(ComposeHeader);
+                page.Content()
+                    //.Section("Receipt")
+                    .AlignCenter()
+                    .Element(ComposeContent);
+                page.Footer() /*.Section("Receipt")*/
+                    .Element(ComposeFooter);
+            });
+        }
+
+        void ComposeHeader(IContainer container) { }
+
+        void ComposeContent(IContainer container) { }
+
+        void ComposeFooter(IContainer container) { }
+
+        public DocumentMetadata GetMetadata() => DocumentMetadata.Default;
+    }
+}
---
+++ server/Server.cs
@@ -0,0 +1,174 @@
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MimeKit;
+using Stripe;
+using Stripe.Checkout;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Threading.Tasks;
+
+public class StripeOptions
+{
+    public string option { get; set; }
+}
+
+namespace server.Controllers
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            WebHost
+                .CreateDefaultBuilder(args)
+                .UseUrls("http://0.0.0.0:4242")
+                .UseWebRoot("public")
+                .UseStartup<Startup>()
+                .Build()
+                .Run();
+        }
+    }
+
+    public class Startup
+    {
+        const string myAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().AddNewtonsoftJson();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // This is your test secret API key.
+            StripeConfiguration.ApiKey =
+                "sk_test_51PY9wlAFhMxzwGjjtnMpqSd7iQcSajbWCsaAq6EIb6cTSKhp3OZ1dGwvEKkBVzKg5UnnH75WmkAFvFlef2jrtDOf00ad9s3hBS";
+            if (env.IsDevelopment())
+                app.UseDeveloperExceptionPage();
+            app.UseCors(policyBuilder =>
+                policyBuilder
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .WithOrigins("https://makamifoundation.com")
+            );
+            app.UseRouting();
+            app.UseStaticFiles();
+            app.UseEndpoints(endpoints => endpoints.MapControllers());
+        }
+    }
+
+    public class DonationRequest
+    {
+        [Required]
+        public string Email { get; set; }
+        public string DonationTowards { get; set; }
+        public string OnBehalfOf { get; set; }
+    }
+
+    [Route("transaction")]
+    [ApiController]
+    public class CheckoutApiController : Controller
+    {
+        [HttpPost("create-checkout-session")]
+        public ActionResult Create(DonationRequest request)
+        {
+            var options = new SessionCreateOptions
+            {
+                LineItems = new List<SessionLineItemOptions>
+                {
+                    new SessionLineItemOptions
+                    {
+                        // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+                        Price = "price_1Q0RYQAFhMxzwGjjLuCSBSzB",
+                        Quantity = 1,
+                    },
+                },
+                Mode = "payment",
+                SuccessUrl = "https://makamifoundation.com/thank-you-for-your-donation/",
+                CancelUrl = "https://makamifoundation.com/Donate",
+                PaymentIntentData = new()
+                {
+                    Description = $"Charitable Donation Number: 84327 7211 RR0001",
+                    Metadata = new() { { "test", "test" } }
+                },
+                CustomerEmail = request.Email,
+            };
+            if (!string.IsNullOrWhiteSpace(request.DonationTowards))
+            {
+                options.PaymentIntentData.Description +=
+                    $", Donation made towards: {request.DonationTowards}";
+            }
+            if (!string.IsNullOrWhiteSpace(request.OnBehalfOf))
+            {
+                options.PaymentIntentData.Description +=
+                    $", Donation made on behalf of: {request.OnBehalfOf}";
+            }
+            var service = new SessionService();
+            Session session = service.Create(options);
+
+            return Ok(new { redirect_url = session.Url });
+        }
+
+        [HttpPost("payment-intent-succeeded")]
+        public async Task<ActionResult> SendReceiptEmail()
+        {
+            var json = await new StreamReader(HttpContext.Request.Body).ReadToEndAsync();
+
+            try
+            {
+                var stripeEvent = EventUtility.ParseEvent(json);
+
+                if (stripeEvent.Type == Events.PaymentIntentSucceeded)
+                {
+                    var intent = stripeEvent.Data.Object as PaymentIntent;
+
+                    MimeMessage email = new MimeMessage();
+                    MailboxAddress from = new MailboxAddress(
+                        "Makami Foundation",
+                        "makamiFoundation@makamicollege.com"
+                    );
+                    email.From.Add(from);
+                    email.Subject = "Donation Receipt to Makami Foundation";
+                    BodyBuilder bodyBuilder = new BodyBuilder();
+                    bodyBuilder.HtmlBody = "<p>Thank you for your donation!</p>";
+                    email.Body = bodyBuilder.ToMessageBody();
+                    SmtpClient client = new SmtpClient();
+
+                    await client.ConnectAsync(
+                        "proxy.makamicollege.com",
+                        25,
+                        SecureSocketOptions.None
+                    );
+
+                    MailboxAddress To = new MailboxAddress("TEST", intent.ReceiptEmail);
+                    email.To.Add(To);
+                    await client.SendAsync(email);
+                    await client.DisconnectAsync(true);
+                    client.Dispose();
+                }
+                else
+                {
+                    // Unexpected event type
+                    Console.WriteLine("Unhandled event type: {0}", stripeEvent.Type);
+                }
+                return Ok();
+            }
+            catch (StripeException e)
+            {
+                return BadRequest();
+            }
+            catch (Exception e) when (e is not StripeException)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+    }
+}
